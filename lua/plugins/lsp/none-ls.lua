44c7ae0e660e6b9974e818b199a9d09a6ab96f4c
Étienne Lafarge <etienne.lafarge@gmail.com>
2024-01-16 18:36:20 +0100

    Fix golang by getting rid of gospel producing wrong diagnostics and nilaway which isn't working

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -11,8 +11,6 @@ return {
     local null_ls = require("null-ls")
 
     local null_ls_utils = require("null-ls.utils")
-    local null_ls_helpers = require("null-ls.helpers")
-    local null_ls_logger = require("null-ls.logger")
 
     mason_null_ls.setup({
       ensure_installed = {
@@ -20,9 +18,7 @@ return {
         "goimports",
         "golangci-lint",
         "golines",
-        "gospel",
         "hadolint",
-        "nilaway",
         "prettierd",
         "stylua", -- lua formatter
         "black", -- python formatter
@@ -40,54 +36,6 @@ return {
 
     -- to setup format on save
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    local nilaway = {
-      name = "nilaway",
-      meta = {
-        url = "https://github.com/uber-go/nilaway",
-        description = "NilAway is a static analysis tool that seeks to help developers avoid nil panics in production by catching them at compile time rather than runtime",
-      },
-      method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-      filetypes = { "go" },
-      generator = null_ls.generator({
-        command = "nilaway",
-        to_stdin = true,
-        from_stderr = false,
-        ignore_stderr = true,
-        multiple_files = true,
-        cwd = null_ls_helpers.cache.by_bufnr(function(params)
-          return null_ls_utils.root_pattern("go.mod")(params.bufname)
-        end),
-        args = { "run", "--fix=false", "--out-format=json" },
-        format = "json",
-        check_exit_code = function(code)
-          return code <= 2
-        end,
-        on_output = function(params)
-          local diags = {}
-          if params.output["Report"] and params.output["Report"]["Error"] then
-            null_ls_logger:warn(params.output["Report"]["Error"])
-            return diags
-          end
-          local issues = params.output["Issues"]
-          if type(issues) == "table" then
-            for _, d in ipairs(issues) do
-              table.insert(diags, {
-                source = string.format("golangci-lint: %s", d.FromLinter),
-                row = d.Pos.Line,
-                col = d.Pos.Column,
-                message = d.Text,
-                severity = null_ls_helpers.diagnostics.severities["warning"],
-                filename = null_ls_utils.path.join(params.cwd, d.Pos.Filename),
-              })
-            end
-          end
-          return diags
-        end,
-      }),
-    }
-
-    null_ls.register(nilaway)
 
     -- configure null_ls
     null_ls.setup({
@@ -128,7 +76,6 @@ return {
         }),
         diagnostics.shellcheck,
         diagnostics.golangci_lint,
-        diagnostics.gospel,
         diagnostics.hadolint,
       },
 
