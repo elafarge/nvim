3700e26b6c1243196674c9d11563132c6f540eb8
Étienne Lafarge <etienne.lafarge@gmail.com>
2024-01-16 13:30:34 +0100

    Go back to Telescope's default mappings and fix ignore patterns to include .circleci files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,18 +8,26 @@ return {
   },
   config = function()
     local telescope = require("telescope")
-    local actions = require("telescope.actions")
 
     telescope.setup({
       defaults = {
         path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
+        file_ignore_patterns = { ".git" },
+      },
+      pickers = {
+        live_grep = {
+          file_ignore_patterns = { "node_modules", ".git", "bin", "obj" },
+          additional_args = function(_)
+            return { "--hidden" }
+          end,
         },
+        find_files = {
+          file_ignore_patterns = { "node_modules", ".git", "bin", "obj" },
+          hidden = true,
+        },
+      },
+      extensions = {
+        "fzf",
       },
     })
 
